3efc10153ba641febab5db9a72efe7fa630064be
Aidan <aidan.lee63@gmail.com>
2018-12-18 22:14:20 +0000

    Replaced implements dynamic with abstracts for haxe 4

--- src/mockatoo/internal/Verification.hx
+++ src/mockatoo/internal/Verification.hx
@@ -6,10 +6,8 @@ import mockatoo.Mockatoo;
 	Dynamic class that is created for verification.
 	Each dynamic method calls to a specific MockMethod.
 */
-class Verification implements Dynamic<Dynamic>
+abstract Verification(VerificationMode) from Dynamic to Dynamic
 {
-	public var mode:VerificationMode;
-
 	public function new(mode:VerificationMode)
 	{
 		this.mode = mode;	
